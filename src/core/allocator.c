9da1cf3d0227fd1c2a809a577e52ff909959ed6d
kyloReneo <saeedderayat2000@gmail.com>
2025-10-14 19:32:00 +0330

    fmalloc init

--- src/core/allocator.c
+++ src/core/allocator.c
@@ -11,7 +11,19 @@ static block_header_t *free_lists[FMALLOC_SIZE_CLASS_COUNT] = {NULL};
 static bool debug_enabled = false;
 
 // Core allocation functions
-void *fmalloc(size_t size) {}
+void *fmalloc(size_t size)
+{
+    if (!memforge_initialized)
+    {
+        memforge_init();
+    }
+
+    // malloc(0) behavior based on glibc doc : If size is 0, then malloc() returns a unique pointer value that can later be successfully passed to free().
+    if (size == 0)
+    {
+        size = 1;
+    }
+}
 void fmfree(void *ptr)
 {
     if (ptr == NULL)
